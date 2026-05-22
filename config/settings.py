06af67ae800a4a076a5e67e4de855656e3977d37
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-05-22 18:10:34 +0900

    ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEYß')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','ajax_app-https://ajaxapp-2cki.onrender.com']
 
 
 # Application definition
