35ef5f87c4514cf6576759c07dcc259fc50b1e0d
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-05-22 16:58:29 +0900

    カリキュラム完了です。

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
+from dj_database_url import parse as dburl
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env=environ.Env()
+env.read_env(os.path.join(BASE_DIR,"env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*u31ejjlte7&g+cl+9@5cvv12v$m80!-(wfc+r8=*+_znz7(s&'
+SECRET_KEY = env('SECRET_KEYß')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -43,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -74,17 +83,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ajax_app',
-        'USER': 'postgres',
-        'PASSWORD': 'admin', # ご自身で設定したパスワード
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+default_dburl="sqlite:///"+str(BASE_DIR/"db.sqlite3")
+DATABASES={
+  "default":config("DATABASE_URL",default_dburl, cast=dburl),
 }
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'ajax_app',
+        #'USER': 'postgres',
+        #'PASSWORD': 'admin', # ご自身で設定したパスワード
+        #'HOST': 'localhost',
+        #'PORT': '5432',
+    #}
+#}
 
 
 # Password validation
@@ -123,6 +135,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+
+STATIC_ROOT=str(BASE_DIR/"staticfiles")
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
